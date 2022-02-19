9cd5877f34fff6385d84efd9e696b128cf62d279
cafe51 <cafecafe51@hotmail.com>
2022-02-19 19:06:12 -0400

    requisitos 7 e 8 resolvidos

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function grade(tamanho) {
+function gradeSizer(tamanho) {
   for (let i = 0; i < tamanho; i += 1) {
     const linha = document.createElement('section');
     const pixelBoard = document.querySelector('#pixel-board');
@@ -13,16 +13,26 @@ function grade(tamanho) {
   }
 }
 
-grade(5);
+gradeSizer(5);
 
-// for (let i = 2; i < l; i += 1) {
-//   document.createElement('section');
-//   document.querySelector('#pixel-board').appendChild(document.createElement('section'));
-//   document.querySelectorAll('section')[i].className = 'line';
-//   for (let j = 0; j < c.length; j += 1) {
-//     let pixel = document.createElement('div');
-//     pixel.className = 'pixel';
-//     let line = document.querySelectorAll('.line')[i];
-//     line.appendChild(pixel);
-//   }
-// }
+const paleta = document.querySelector('#color-palette');
+const grade = document.querySelector('#pixel-board');
+
+function trocaSeletor(evt) {
+  const selecionado = document.querySelector('.selected');
+  selecionado.classList.remove('selected');
+  evt.target.classList.add('selected');
+}
+
+paleta.addEventListener('click', trocaSeletor);
+
+function pinta(evt) {
+  const selecionado = document.querySelector('.selected');
+  const corNova = selecionado.classList[1];
+  const alvo = evt.target;
+  if (alvo.classList[0] === 'pixel') {
+    alvo.style.backgroundColor = corNova;
+  }
+}
+
+grade.addEventListener('click', pinta);
