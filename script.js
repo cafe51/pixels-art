1434f021d7163c90ca90b346f81e422a03f56b19
cafe51 <cafecafe51@hotmail.com>
2022-02-19 21:32:17 -0400

    requisito 9 resolvido

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+const body = document.querySelector('body');
+const paleta = document.querySelector('#color-palette');
+const grade = document.querySelector('#pixel-board');
+
+function criaSecaoClear() {
+  const secao = document.createElement('section');
+  secao.setAttribute('id', 'section-clear');
+  body.appendChild(secao);
+}
+
+function criaBotaoClear() {
+  const botaoClear = document.createElement('button');
+  botaoClear.innerText = 'Limpar';
+  botaoClear.setAttribute('id', 'clear-board');
+  const secaoClear = document.querySelector('#section-clear');
+  secaoClear.appendChild(botaoClear);
+}
+
 function gradeSizer(tamanho) {
   for (let i = 0; i < tamanho; i += 1) {
     const linha = document.createElement('section');
@@ -13,18 +31,11 @@ function gradeSizer(tamanho) {
   }
 }
 
-gradeSizer(5);
-
-const paleta = document.querySelector('#color-palette');
-const grade = document.querySelector('#pixel-board');
-
 function trocaSeletor(evt) {
   const selecionado = document.querySelector('.selected');
   selecionado.classList.remove('selected');
   evt.target.classList.add('selected');
 }
-
-paleta.addEventListener('click', trocaSeletor);
 
 function pinta(evt) {
   const selecionado = document.querySelector('.selected');
@@ -35,4 +46,27 @@ function pinta(evt) {
   }
 }
 
+function clear() {
+  const pixeis = document.querySelectorAll('.pixel');
+  for (let i = 0; i < pixeis.length; i += 1) {
+    pixeis[i].style.backgroundColor = 'rgb(255, 255, 255)';
+  }
+}
+
+function botaoEventoClear() {
+  const botao = document.querySelector('#clear-board');
+  botao.addEventListener('click', clear);
+}
+
+criaSecaoClear();
+
+criaBotaoClear();
+
+gradeSizer(5);
+
+paleta.addEventListener('click', trocaSeletor);
+
 grade.addEventListener('click', pinta);
+
+botaoEventoClear();
+
